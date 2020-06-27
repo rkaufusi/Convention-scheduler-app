78a9cc092f8469813d407c8af394f6745a57313a
rkaufusi <rhettkaufusi@gmail.com>
2020-06-26 17:24:58 -0700

    6/26/2020 commit

--- app/src/main/java/com/example/rkwguapp/AppDatabase.java
+++ app/src/main/java/com/example/rkwguapp/AppDatabase.java
@@ -1,13 +1,57 @@
 package com.example.rkwguapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.Date;
+
 
 @Database(entities = {TermEntity.class, CourseEntity.class, AssessmentEntity.class}, version = 1)
+@TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase instance;
 
     public abstract TermDao termDao();
+    public abstract CourseDao courseDao();
+    public abstract AssessmentDao assessmentDao();
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class, "app_database").fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
+// new code
+private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+
+};
+
+        private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+            private TermDao termDao;
+
+        private PopulateDbAsyncTask(AppDatabase db) {
+            termDao = db.termDao();
+        }
+            @Override
+            protected Void doInBackground(Void... voids) {
+                termDao.insert(new TermEntity("Title 1", new Date(2020), new Date(2020)));
+                termDao.insert(new TermEntity("Title 2", new Date(2020), new Date(2020)));
+                termDao.insert(new TermEntity("Title 3", new Date(2021), new Date(2021)));
+                return null;
+            }
+    }
 
 }
