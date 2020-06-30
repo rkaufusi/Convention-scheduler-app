5c1464da8210a9815feb56a6009ec7a451ad51a2
rkaufusi <rhettkaufusi@gmail.com>
2020-06-30 14:19:13 -0700

    6/30/2020 commit
    working on onDateSet

--- app/src/main/java/com/example/rkwguapp/AddTerm.java
+++ app/src/main/java/com/example/rkwguapp/AddTerm.java
@@ -1,13 +1,22 @@
 package com.example.rkwguapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class AddTerm extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AddTerm extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private EditText editTermTitle;
     private TextView editStartDate;
     private TextView editEndDate;
@@ -23,6 +32,37 @@ public class AddTerm extends AppCompatActivity {
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.close_button);
         setTitle("Add Term");
+
+        Button button = (Button) findViewById(R.id.start_date_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                DialogFragment termStartPicker = new TermStartFragment();
+                termStartPicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.end_date_button);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                DialogFragment termEndPicker = new TermEndFragment();
+                termEndPicker.show(getSupportFragmentManager(), "end date picker");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String termStartString = DateFormat.getDateInstance().format(c.getTime());
+        TextView textView = (TextView) findViewById(R.id.start_date_textview);
+        textView.setText(termStartString);
+
     }
 
     @Override
